a621bd470d29d90f317ac4b8adc36921f9b541a9
Gomding <backchida@gmail.com>
2020-11-25 19:48:10 +0900

    같은 타입의 빈이 여러개 일 때 @Primary 애너테이션을 사용해서 주입할 빈을 선택함

--- src/main/java/com/example/demospring51/BookRepository.java
+++ src/main/java/com/example/demospring51/BookRepository.java
@@ -1,6 +1,4 @@
 package com.example.demospring51;
 
-import org.springframework.stereotype.Repository;
-
 public interface BookRepository {
 }
--- src/main/java/com/example/demospring51/ParkBookRepository.java
+++ src/main/java/com/example/demospring51/ParkBookRepository.java
@@ -1,7 +1,11 @@
 package com.example.demospring51;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class ParkBookRepository {
+/*
+@Primary를 붙여주면 사용할 빈을 marking 할 수 있습니다.
+ */
+@Repository @Primary
+public class ParkBookRepository implements BookRepository{
 }
