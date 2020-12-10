3239010db3bb8a9a1c75987a9e61d590eb8db512
Gomding <backchida@gmail.com>
2020-12-10 18:06:22 +0900

    @SpringBootApplication 애너테이션이 demospring51 패키지에 있기때문에 out 패티지는 ComponentScan의 대상이 되지않음

--- src/main/java/com/example/demospring51/ParkBookRepository.java
+++ src/main/java/com/example/demospring51/ParkBookRepository.java
@@ -1,11 +1,10 @@
 package com.example.demospring51;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 /*
 @Primary를 붙여주면 사용할 빈을 marking 할 수 있습니다.
  */
 @Repository
-public class ParkBookRepository implements BookRepository{
+public class ParkBookRepository implements BookRepository {
 }
