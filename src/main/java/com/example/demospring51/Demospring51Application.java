10b7f151cab069d2dc081b8f563dbe47b2e84756
Gomding <backchida@gmail.com>
2020-12-10 18:37:00 +0900

    펑션을 사용한 빈 등록
    프로그래밍 적인 컨트롤이 가능해짐, 애플리케이션 구동시 성능상의 이점이 있음

--- src/main/java/com/example/demospring51/Demospring51Application.java
+++ src/main/java/com/example/demospring51/Demospring51Application.java
@@ -2,14 +2,28 @@ package com.example.demospring51;
 
 import com.example.out.MyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class Demospring51Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Demospring51Application.class, args);
-    }
+    @Autowired
+    MyService myService;
 
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Demospring51Application.class);
+        app.addInitializers((ApplicationContextInitializer<GenericApplicationContext>) ctx -> {
+            ctx.registerBean(MyService.class);
+            ctx.registerBean(ApplicationRunner.class, () -> args1 -> System.out.println("Funtional Bean Definition!!"));
+        });
+        app.run(args);
+    }
 }
