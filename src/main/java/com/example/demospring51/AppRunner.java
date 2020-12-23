389f5b53d93e015c67ae7743cd227902fcdabfaf
Gomding <backchida@gmail.com>
2020-12-23 18:25:56 +0900

    IoC컨테이너 ApplicationEventPublisher 스프링 이벤트 프로그래밍
    이벤트 만들기_1 : ApplicationEvent 상속

--- src/main/java/com/example/demospring51/AppRunner.java
+++ src/main/java/com/example/demospring51/AppRunner.java
@@ -3,22 +3,17 @@ package com.example.demospring51;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.MessageSource;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
-import java.util.Locale;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    MessageSource messageSource;
+    ApplicationEventPublisher eventPublisher;
 
     public void run(ApplicationArguments args) throws Exception {
-        while (true) {
-            System.out.println(messageSource.getMessage("greeting", new String[]{"park"}, Locale.KOREA));
-            System.out.println(messageSource.getMessage("greeting", new String[]{"park"}, Locale.getDefault()));
-            Thread.sleep(1000L);
-        }
+        eventPublisher.publishEvent(new MyEvent(this, 100));
     }
 }
---
+++ src/main/java/com/example/demospring51/MyEventHandler.java
@@ -0,0 +1,13 @@
+package com.example.demospring51;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyEventHandler implements ApplicationListener<MyEvent> {
+
+    @Override
+    public void onApplicationEvent(MyEvent myEvent) {
+        System.out.println("이벤트 받았다. 데이터는 " + myEvent.getData());
+    }
+}
