5a2543366c4fee162eadd3f9d3ce607ae4585a30
Gomding <backchida@gmail.com>
2020-12-23 17:43:12 +0900

    Environment 프로퍼티

--- src/main/java/com/example/demospring51/AppRunner.java
+++ src/main/java/com/example/demospring51/AppRunner.java
@@ -1,13 +1,12 @@
 package com.example.demospring51;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
 
 @Component
 public class AppRunner implements ApplicationRunner {
@@ -18,9 +17,13 @@ public class AppRunner implements ApplicationRunner {
     @Autowired
     BookRepository bookRepository;
 
+    @Value("${app.name}")
+    String appName;
+
     public void run(ApplicationArguments args) throws Exception {
         Environment environment = ctx.getEnvironment();
-        System.out.println(Arrays.toString(environment.getActiveProfiles()));
-        System.out.println(Arrays.toString(environment.getDefaultProfiles()));
+        System.out.println(environment.getProperty("app.name"));
+        System.out.println(environment.getProperty("app.about"));
+        System.out.println(appName);
     }
 }
--- src/main/java/com/example/demospring51/Demospring51Application.java
+++ src/main/java/com/example/demospring51/Demospring51Application.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/app.properties")
 public class Demospring51Application {
 
     public static void main(String[] args) {
