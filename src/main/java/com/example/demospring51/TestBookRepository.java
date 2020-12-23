425fd267a3a15a92d8ea600c7535d41dc5263237
Gomding <backchida@gmail.com>
2020-12-23 17:04:18 +0900

    Environment 프로파일 정의하기 - 클래스에 정의_2
    @Component (@Service, @Repository, 등등)
    @Profile("test")

--- src/main/java/com/example/demospring51/TestBookRepository.java
+++ src/main/java/com/example/demospring51/TestBookRepository.java
@@ -1,4 +1,9 @@
 package com.example.demospring51;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Profile("test")
 public class TestBookRepository implements BookRepository{
 }
