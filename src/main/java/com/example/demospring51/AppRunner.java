d6e1cca773655b7ba3cfd8fda7fe965c69d0a20d
Gomding <backchida@gmail.com>
2021-01-06 02:26:55 +0900

    14. Validation

--- src/main/java/com/example/demospring51/AppRunner.java
+++ src/main/java/com/example/demospring51/AppRunner.java
@@ -1,25 +1,40 @@
 package com.example.demospring51;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.util.Arrays;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
+    @Qualifier("defaultValidator")
     @Autowired
-    ResourceLoader resourceLoader;
+    Validator validator;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(resourceLoader.getClass());
+        System.out.println(validator.getClass());
 
-        Resource resource = resourceLoader.getResource("test.txt");
-        System.out.println(resource.getClass());
-        System.out.println(resource.exists());
-        System.out.println(resource.getDescription());
+        Event event = new Event();
+        event.setLimit(-1);
+        event.setEmail("aaa2");
+        Errors errors = new BeanPropertyBindingResult(event, "event");
+
+        validator.validate(event, errors);
+
+        System.out.println(errors.hasErrors());
+
+        errors.getAllErrors().forEach(e -> {
+            System.out.println("==== error code ====");
+            Arrays.stream(e.getCodes()).forEach(System.out::println);
+            System.out.println(e.getDefaultMessage());
+        });
     }
 }
