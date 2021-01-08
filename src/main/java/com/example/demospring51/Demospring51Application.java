0aa5df1cbeea3c9fc2733fcd78931bfaefef4a37
Gomding <backchida@gmail.com>
2021-01-08 17:05:41 +0900

    16. 데이터 바인딩 추상화 : Converter 와 Fomatter
    Converter
    - S타입을 T타입으로 변환할 수 있는 매우 일반적인 변환기
    - 상태 정보 없음 == Stateless == 쓰레드세이프
    - ConverterRegistry 에 등록해서 사용
    
    Formatter
    - PropertyEditor 대체제
    - Object와 String 간의 변환을 담당한다.
    - 문자열을 Locale에 따라 다국화하는 기능도 제공한다.(optional)
      FormatterRegistry에 등록해서 사용
    
    ConversionService
    - 실제 변환 작업은 이 인터페이스를 통해서 쓰레드-세이프하게 사용할 수 있음.
    - 스프링 MVC, 빈 (value) 설정, spEL에서 사용한다.
    - DefaultFormattingConversionService
    - FormatterRegistry
    - ConversionService
    - 여러 기본 컨버터와 포매터 등록 해 줌.
    
    스프링 부트
    - 웹 애플리케이션인 경우에 DefaultFormattingConversionService 를 상속하여 만든 WebConversionService를 빈으로 등록해준다.
    - Formatter와 Convertter 빈을 찾아 자동으로 등록해 준다.

--- src/main/java/com/example/demospring51/Demospring51Application.java
+++ src/main/java/com/example/demospring51/Demospring51Application.java
@@ -2,7 +2,6 @@ package com.example.demospring51;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 public class Demospring51Application {
--- src/test/java/com/example/demospring51/EventControllerTest.java
+++ src/test/java/com/example/demospring51/EventControllerTest.java
@@ -10,7 +10,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+//@WebMvcTest({EventFormatter.class, EventController.class})
+@WebMvcTest({EventConverter.StringToEventConverter.class, EventController.class})
 class EventControllerTest {
 
     @Autowired
