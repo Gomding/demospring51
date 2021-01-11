b8fcc2085b26474f32e2ddb50794a3e9783068b5
Gomding <backchida@gmail.com>
2021-01-11 20:40:10 +0900

    19. 스프링 AOP: 개념 소개
    스프링 AOP 특징
    - 프록시 기반의 AOP 구현체
    - 스프링 빈에만 AOP를 적용할 수 있다.
    - 모든 AOP 기능을 제공하는 것이 목적이 아니라, 스프링 IoC와 연동하여 엔터프라이즈
    - 애플리케이션에서 가장 흔한 문제에 대한 해결책을 제공하는 것이 목적.
    
    프록시 패턴
    - 왜? (기존 코드 변경 없이) 접근 제어 또는 부가 기능 추가
    - 기존 코드를 건드리지 않고 성능을 측정해 보자(프록시 패턴으로)
    
    문제점
    - 매번 프록시 클래스를 작성해야 하는가?
    - 여러 클래스 여러 메소드에 적용하려면? (결국 반복되는 코드가 발생함)
    - 객체들 관계도 복잡해짐
    
    그래서 등장한 것이 스프링 AOP
    - 스프링 IoC 컨테이너가 제공하는 기반 시설과 Dynamic 프록시를 사용하여 여러 복잡한 문제 해결
    - 동적 프록시 : 동적으로 프록시 객체 생성하는 방법
        * 자바가 제공하는 방법은 인터페이스 기반 프록시 생성.
        * CGlib은 클래스 기반 프록시도 지원
    - 스프링 IoC: 기존 빈을 대체하는 동적 프록시 빈을 만들어 등록 시켜준다.
        * 클라이언트 코드 변경 없음.
        * AbstractAutoProxyCreator implements BeanPostProcessor

--- src/main/java/com/example/demospring51/Demospring51Application.java
+++ src/main/java/com/example/demospring51/Demospring51Application.java
@@ -1,12 +1,16 @@
 package com.example.demospring51;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demospring51Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Demospring51Application.class, args);
+        SpringApplication app = new SpringApplication(Demospring51Application.class);
+        app.setWebApplicationType(WebApplicationType.NONE);
+        app.run(args);
+        // SpringApplication.run(Demospring51Application.class);
     }
 }
